0d95380bfaae9e764727179f2c6026bb78b3d323
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-01 17:40:01 +0530

    Add FlatButtons for all 7 notes

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,61 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              child: Text('Click Me'),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red[600],
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange[600],
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow[600],
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.green[600],
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.teal[600],
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.blue[600],
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple[600],
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
